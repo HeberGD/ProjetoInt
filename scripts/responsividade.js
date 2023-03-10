83e3ad18d2aea8127024260ee82292945368d51e
MegTteg <113463960+HeberGD@users.noreply.github.com>
2023-03-10 20:13:51 -0300

    correçoes gerais de responsividade 2

--- scripts/responsividade.js
+++ scripts/responsividade.js
@@ -3,22 +3,23 @@
 $(document).ready(function() {
     nav();
     formularios();
-
+    navmobile();
 
 });
 $(window).resize(function() {
     nav();
     formularios();
-
+    navmobile();
 
 });
 
 
 // FUNÇÕES 
 function nav() {
-    if (document.body.clientWidth < 800) {    
+    if (document.body.clientWidth < 600) {    
         $("iframe").addClass("tam");
         $('.cassoselInfo').addClass("hide");
+        
     } else {    
         $("iframe").removeClass("tam");
         $('.cassoselInfo').removeClass("hide");
@@ -26,7 +27,7 @@ function nav() {
 };
 
 function formularios() {
-    if (document.body.clientWidth < 800) {    
+    if (document.body.clientWidth < 600) {    
         $(".bodydiv").addClass("mobilesz");
         
     } else {    
@@ -35,7 +36,15 @@ function formularios() {
     }
 };
 
-
+function navmobile() {
+    if (document.body.clientWidth < 600) {    
+        $(".dsk").addClass("menumobile");
+        
+    } else {    
+        $(".dsk").removeClass("menumobile");
+       
+    }
+};
 
 
 
