a46bab44457ad9298421d7d30b524f6e1c0af27e
HeberGG <113463960+HeberGD@users.noreply.github.com>
2023-05-08 19:36:29 -0300

    final version

---
+++ contatos/cont_pro.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include_once("../login/conexao.php");
+
+$nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_STRING);
+$mensagem= filter_input(INPUT_POST,'mensagem', FILTER_SANITIZE_STRING);
+$tipo_mensagem= filter_input(INPUT_POST,'tipo_mensagem', FILTER_SANITIZE_STRING);
+
+$result_contato = "INSERT INTO contato(nome, email, tipo_mensagem, mensagem)
+ VALUES('$nome', '$email','$tipo_mensagem' ,'$mensagem')";
+$resultado = mysqli_query($conn, $result_contato);
+
+if ($conn->affected_rows == 1) {
+    $_SESSION['msg'] = "<h2><p style='color: green;'>Mensagem Enviada</p></h2>";
+    
+} else {
+    $_SESSION['msg'] = "<h2><p style='color:red';>Mensagem Não Enviada, Verifique se todos os campos foram preenchidos correntamente!</p></h2>";
+}
+header("Location:contatos.php"); 
+?>
